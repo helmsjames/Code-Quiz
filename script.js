78a2ef0609cae3d0b7d91fcf4d98347c5d1857cb
James Helms <helms.james@gmail.com>
2020-01-29 22:00:28 -0600

    adding html, js, and css files which are all a work in progress. This hw assignment is not complete.

---
+++ script.js
@@ -0,0 +1,20 @@
+// var score val = 0;
+
+
+// var startBtn = button("click");
+// var startBtn = document.getElementsByClassName("start-button");
+// onclick.apply
+// console.log(startBtn)
+
+var answerClick = document.getElementById("choices");
+answerClick.addEventListener("click", function(event){
+console.log(this);
+console.log(event)
+})
+
+
+Object
+var qas
+q: 
+choices: 
+answer: 
